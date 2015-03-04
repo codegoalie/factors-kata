c2e0491e2809b722f5e180418ebdcc20f05b754e
Chris Marshall <chris@chrismar035.com>
2015-03-04 13:37:49 -0500

    Day 1

---
+++ factors_test.go
@@ -0,0 +1,34 @@
+package factors
+
+import "testing"
+
+func TestGenerate(t *testing.T) {
+	cases := make(map[int][]int)
+	cases[1] = []int{}
+	cases[2] = []int{2}
+	cases[3] = []int{3}
+	cases[4] = []int{2, 2}
+	cases[6] = []int{2, 3}
+	cases[8] = []int{2, 2, 2}
+	cases[9] = []int{3, 3}
+	for input, expected := range cases {
+		got := Generate(input)
+		if !sliceEquals(expected, got) {
+			t.Errorf("Generate(%d) == %v, want %v", input, got, expected)
+		}
+	}
+}
+
+func sliceEquals(x, y []int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for i, lhs := range x {
+		if lhs != y[i] {
+			return false
+		}
+	}
+
+	return true
+}
